3af01b1fc4bbe52dcaa3f9b951d35c58e6fa6072
Matthew Bissenden <matt.bissenden@gmail.com>
2014-01-12 12:46:55 +0000

    Added initial client file.

---
+++ src/client.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file client.cpp
+ *
+ * @desc The entry-point to the world of logs c++ client.
+ *
+ * @author Matthew Bissenden
+ */
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
