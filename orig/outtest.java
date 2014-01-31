bf681e570694a00044fa7a946358df799d94f322
Matthew Bissenden <matt.bissenden@gmail.com>
2014-01-31 14:58:38 +0000

    Saving/

--- orig/outtest.java
+++ orig/outtest.java
@@ -6,56 +6,74 @@ class outtest
 
         com.wol3.client.data.BinaryCombatLog        combatLog = new com.wol3.client.data.BinaryCombatLog();
         com.wol3.client.data.TextualCombatLogParser parser = new com.wol3.client.data.TextualCombatLogParser();
-        java.nio.ByteBuffer                         buffer = java.nio.ByteBuffer.allocate(0x4000000);
-        java.nio.ByteBuffer                         buffer1 = java.nio.ByteBuffer.allocate(0x4000000);
-        java.nio.ByteBuffer                         buffer2 = java.nio.ByteBuffer.allocate(0x4000000);
-        java.nio.ByteBuffer                         buffer3 = java.nio.ByteBuffer.allocate(0x4000000);
-        java.nio.ByteBuffer                         buffer4 = java.nio.ByteBuffer.allocate(0x4000000);
-        java.nio.ByteBuffer                         buffer5 = java.nio.ByteBuffer.allocate(0x4000000);
-        java.nio.ByteBuffer                         buffer6 = java.nio.ByteBuffer.allocate(0x4000000);
+        java.nio.ByteBuffer                         bb     = java.nio.ByteBuffer.allocate(0x1fb);
+        int                                         fromActor = 0;
+        int                                         fromEvent = 0;
+        int                                         fromEntry = 0;
 
         try
         {
             parser.parseLine(combatLog, line, 1);
             combatLog.finish();
 
-            System.out.println("1");
+            /* The following is copied code from     BinaryCombatLog::public void
+             * writeTo(java.nio.ByteBuffer bb, int fromActor, int fromEvent,
+             * int fromEntry)                           |
+             */
+            com.wol3.util.perf.SimpleProfiler.staticEnter("BCL:writeTo");
+            bb.putLong(0x1fb902131029L);
+            bb.rewind();
+System.out.println("1");
+System.out.println(bytesToHexString(bb.array()));
+bb = java.nio.ByteBuffer.allocate(0x1fb);
 
-            combatLog.actorPool.writeTo(buffer, com.wol3.client.data.BinaryCombatLog.Format.valueOf("PATCH_5_4_2"),0);
-            System.out.println(new String(buffer.array()));
+            java.io.ByteArrayOutputStream bytesOut = new java.io.ByteArrayOutputStream();
+            try
+            {
+                combatLog.properties.setProperty("format", combatLog.format.name());
+                combatLog.properties.store(bytesOut, "");
+            }
+            catch (java.io.IOException e)
+            {
+                throw new RuntimeException(e);
+            }
+            com.wol3.util.ByteBufferUtils.putBytes(bb, bytesOut.toByteArray());
+System.out.println("2");
+System.out.println(bytesToHexString(bb.array()));
+bb = java.nio.ByteBuffer.allocate(0x1fb);
 
-            System.out.println("2");
+            combatLog.actorPool.writeTo(bb, combatLog.format, fromActor);
+System.out.println("3");
+System.out.println(bytesToHexString(bb.array()));
+bb = java.nio.ByteBuffer.allocate(0x1fb);
+            combatLog.eventPool.writeTo(bb, fromEvent);
+System.out.println("4");
+System.out.println(bytesToHexString(bb.array()));
+bb = java.nio.ByteBuffer.allocate(0x1fb);
+            combatLog.entryList.writeTo(bb, combatLog.actorPool, fromEntry);
+System.out.println("5");
+System.out.println(bytesToHexString(bb.array()));
+bb = java.nio.ByteBuffer.allocate(0x1fb);
 
-            buffer = java.nio.ByteBuffer.allocate(0x4000000);
-            combatLog.eventPool.writeTo(buffer2, 0);
-            System.out.println(new String(buffer2.array()));
-
-            System.out.println("3");
-
-            buffer = java.nio.ByteBuffer.allocate(0x4000000);
-            combatLog.entryList.writeTo(buffer3, combatLog.actorPool, 0);
-            System.out.println(new String(buffer3.array()));
-
-            System.out.println("4");
-
-            buffer = java.nio.ByteBuffer.allocate(0x4000000);
-            combatLog.stateList.writeToBuffer(buffer4, combatLog, 0);
-            System.out.println(new String(buffer4.array()));
-
-            System.out.println("5");
-
-            buffer = java.nio.ByteBuffer.allocate(0x4000000);
-            combatLog.writeTo(buffer5);
-            System.out.println(new String(buffer5.array()));
-
-            System.out.println("6");
+            if (combatLog.format.hasNewSubjectStates)
+                combatLog.stateList.writeToBuffer(bb, combatLog, fromEntry);
+System.out.println("6");
+System.out.println(bytesToHexString(bb.array()));
+bb = java.nio.ByteBuffer.allocate(0x1fb);
+            com.wol3.util.perf.SimpleProfiler.staticExit();
         }
         catch(Exception e)
         {
             System.out.println(e);
         }
-
-
-
     }
+
+    public static String bytesToHexString(byte[] bytes){
+        StringBuilder sb = new StringBuilder();
+        for(byte b : bytes){
+            sb.append(String.format("%02x", b&0xff));
+        }
+        return sb.toString();
+    }
+
 }
