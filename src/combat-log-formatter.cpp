57e06b924260942b0225cda3c479de66057847fc
Matthew Bissenden <matt.bissenden@gmail.com>
2014-02-05 14:34:40 +0000

    Saving.

--- src/combat-log-formatter.cpp
+++ src/combat-log-formatter.cpp
@@ -10,7 +10,7 @@
 
 namespace WoL
 {
-    ~CombatLogFormatter::CombatLogFormatter()
+    CombatLogFormatter::~CombatLogFormatter()
     {
     }
 }
--- src/wol-combat-log-formatter.cpp
+++ src/wol-combat-log-formatter.cpp
@@ -17,6 +17,25 @@ namespace WoL
 
     std::string WolCombatLogFormatter::formatLog(CombatLog &combatLog)
     {
+        /*
+         * - Magic number
+         * - Properties (datetime, client version, timezone, format)
+         * - Actors:
+         *      - ff00ff00ff00ff00 MAGIC NUMBER
+         *      - The index from which the Actor pool is being uploaded
+         *      - Actor pool:
+         *          - Actor names separated by newlines
+         *          - Actor guids separated for newlines
+         *          - Actor identifiers (as indexed from the Actor list)
+         *          - Actor flags
+         *          - IF raidflags exist)
+         *              - Actor raidflags
+         *
+         * - Events
+         *      - aeaeaeaeaeaeaeae MAGIC NUMBER
+         *      - The index from which Events are being sent
+         *      - Event pool:
+         *          -
         return "";
     }
 }
